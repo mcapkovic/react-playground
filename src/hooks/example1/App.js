c87dc046b2fccbedff065409250703935014fced
Matej Capkovic <mcapkovic@gmail.com>
2019-10-10 16:16:29 +0200

    Add coments

--- src/hooks/example1/App.js
+++ src/hooks/example1/App.js
@@ -2,27 +2,30 @@ import React, { useState, useCallback, useMemo } from "react";
 import Page from "../../common/Page";
 
 function memoDoSomething(message = "default") {
-  console.log( 'memo updated, message: '+ message);
-  return 'hi from memo, message: ' + message
+  console.log("memo updated, message: " + message);
+  return "hi from memo, message: " + message;
 }
 
 function callbackDoSomething(message = "default") {
-  console.log('callback called, message: '+ message);
+  console.log("callback called, message: " + message);
 }
 
 function SomethingL2(props) {
-  const memo = useMemo(memoDoSomething, [props.input1]);
-  const memo2 = useMemo(() => memoDoSomething(props.input1), [props.input1]);
-  
-  const callback = useCallback(callbackDoSomething, [props.input1]);
-  const callback2 = useCallback(() => callbackDoSomething(props.input1), [props.input1]);
+  const callback = useCallback(callbackDoSomething, [props.input1]); // callbackDoSomething function is stored in callback
+  const callback2 = useCallback(() => callbackDoSomething(props.input1), [
+    props.input1
+  ]); // callbackDoSomething function is stored in callback2
+
+  const memo = useMemo(memoDoSomething, [props.input1]); // memoDoSomething function is executed and result is stored in memo
+  const memo2 = useMemo(() => memoDoSomething(props.input1), [props.input1]); // memoDoSomething function is executed and result is stored in memo2
+  // useCallback(fn, deps) is equivalent to useMemo(() => fn, deps)
 
   return (
     <div>
-      <button onClick={() => console.log(memo)}>memo</button>
-      <button onClick={() => console.log(memo2)}>memo2</button>
       <button onClick={() => callback()}>callback</button>
       <button onClick={() => callback2()}>callback2</button>
+      <button onClick={() => console.log(memo)}>memo</button>
+      <button onClick={() => console.log(memo2)}>memo2</button>
     </div>
   );
 }
@@ -35,7 +38,7 @@ function ContextE1(props) {
     <Page {...props}>
       <input value={input1} onChange={event => setInput1(event.target.value)} />
       <input value={input2} onChange={event => setInput2(event.target.value)} />
-      <SomethingL2 input1={input1} input2={input2}/>
+      <SomethingL2 input1={input1} input2={input2} />
     </Page>
   );
 }

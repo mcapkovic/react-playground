c2b0d87dfac5d7e1f64bd47b9886ef014ba64cee
Matej Capkovic <mcapkovic@gmail.com>
2020-06-24 08:32:59 +0200

    Add result block

--- src/components/basic/example4/App.js
+++ src/components/basic/example4/App.js
@@ -13,7 +13,7 @@ const person = {
 };
 
 const person2 = person;
-const person3 = {...person};
+const person3 = { ...person };
 
 function Example(props) {
   const [ignored, forceUpdate] = useReducer((x) => x + 1, 0);
@@ -22,21 +22,25 @@ function Example(props) {
 const person = {
   name: '${person.name}',
   info: {
-    age: 22,
+    age: ${person.info.age},
     height: '170cm',
   }
 }
 const person2 = person;
 const person3 = {...person};
 
-person.name = 
-person2.name = 
-person3.name = 
 
+
+
+
+// orange values are editable
+  `;
+
+  const jsExapmple2 = `
 // person = {
 //  name: '${person.name}',
 //  info: {
-//    age: 22,
+//    age: ${person.info.age},
 //    height: '170cm',
 //  }
 // }
@@ -44,7 +48,7 @@ person3.name =
 // person2 = {
 //  name: '${person2.name}',
 //  info: {
-//    age: 22,
+//    age: ${person2.info.age},
 //    height: '170cm',
 //  }
 // }
@@ -52,39 +56,73 @@ person3.name =
 // person3 = {
 //  name: '${person3.name}',
 //  info: {
-//    age: 22,
+//    age: ${person3.info.age},
 //    height: '170cm',
 //  }
 // }
-  `;
+    `;
   return (
     <Page {...props}>
-      <div className="code">
-        <PrismCode code={jsExapmple} language="js" plugins={["line-numbers"]} />
-        <input
-          className="code__input1"
-          value={person.name}
-          onChange={(e) => {
-            person.name = e.target.value;
-            forceUpdate();
-          }}
-        />
-        <input
-          className="code__input2"
-          value={person2.name}
-          onChange={(e) => {
-            person2.name = e.target.value;
-            forceUpdate();
-          }}
-        />
-         <input
-          className="code__input3"
-          value={person3.name}
-          onChange={(e) => {
-            person3.name = e.target.value;
-            forceUpdate();
-          }}
-        />
+      <div className="example">
+        <div className="code">
+          <PrismCode
+            code={jsExapmple}
+            language="js"
+            plugins={["line-numbers"]}
+          />
+          <code className="code__editable-parts">
+            <div>
+              person.name ={" "}
+              <input
+                value={person.name}
+                onChange={(e) => {
+                  person.name = e.target.value;
+                  forceUpdate();
+                }}
+              />
+            </div>
+            <div>
+              person2.name ={" "}
+              <input
+                value={person2.name}
+                onChange={(e) => {
+                  person2.name = e.target.value;
+                  forceUpdate();
+                }}
+              />
+            </div>
+
+            <div>
+              person3.name ={" "}
+              <input
+                value={person3.name}
+                onChange={(e) => {
+                  person3.name = e.target.value;
+                  forceUpdate();
+                }}
+              />
+            </div>
+
+            <div>
+              person.info.age ={" "}
+              <input
+                value={person.info.age}
+                onChange={(e) => {
+                  person.info.age = e.target.value;
+                  forceUpdate();
+                }}
+              />
+            </div>
+          </code>
+        </div>
+
+        <div className="result">
+          <PrismCode
+            code={jsExapmple2}
+            language="js"
+            plugins={["line-numbers"]}
+          />
+        </div>
       </div>
     </Page>
   );
